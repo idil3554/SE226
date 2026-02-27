318cd550fd98ffbdb0aaf3370eaeacbc9f6841a2
idil3554 <busezengin38@gmail.com>
2026-02-27 17:14:21 +0300

    Delete time_logic.py

--- time_logic.py
+++
@@ -1,8 +0,0 @@
-total_seconds = int(input("Enter total seconds"))
-hours = total_seconds // 3600
-remaining = total_seconds % 3600
-
-minutes = remaining // 60
-seconds = remaining % 60
-
-print(str(total_seconds) + " seconds is " + str(hours) + " hours, " + str(minutes) + " minutes, and " + str(seconds) + " seconds." )
